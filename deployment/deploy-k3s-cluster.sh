53604423e044da6b8468edb719422dc4f604b4a2
Floyd Everest <me@floydeverest.com>
2021-05-10 22:19:54 +1000

    fixed kubeconf dir

--- deployment/deploy-k3s-cluster.sh
+++ deployment/deploy-k3s-cluster.sh
@@ -3,4 +3,4 @@ source .openrc
 export ANSIBLE_HOST_KEY_CHECKING=False
 ansible-playbook ./deployment/deploy-os-hosts.yaml
 ansible-playbook -i ./deployment/openstack_inventory.py ./deployment/configure-k3s.yaml
-export KUBECONFIG=$(pwd)/kubeconfig
+export KUBECONFIG=$(pwd)/deployment/kubeconfig
