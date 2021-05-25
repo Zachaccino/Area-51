cc4aba9b5499a00739b70283caa266f3f65e887f
Jingyuan Tu <jingyuantu@outlook.com>
2021-05-26 00:46:48 +1000

    deployment config

--- frontend/src/Address.js
+++ frontend/src/Address.js
@@ -7,8 +7,8 @@
  */
 
 function Address() {
-    // return "http://" + window.location.hostname + ":30003";
-    return "http://127.0.0.1:5000";
+    return "http://" + window.location.hostname + ":30003";
+    // return "http://127.0.0.1:5000";
 }
 
 export default Address;
